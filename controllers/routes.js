76110da7dcaec79aa0aa198a8dac5e065717694f
ariccio3 <ar3home@gmail.com>
2017-10-17 14:06:20 -0700

    File Updates

--- controllers/routes.js
+++ controllers/routes.js
@@ -9,7 +9,7 @@ var nodemailer = require('nodemailer');
 router.get("/", function(req, res) {
     res.render("index");
 });
-
+// POST request to email index.html form to Jon
 router.post("/", (req, res) => {
   console.log(req.body);
   const output = `
@@ -54,13 +54,63 @@ router.post("/", (req, res) => {
         console.log('Message sent: %s', info.messageId);
         var chargeType = `${req.body.chargeType}`;
         if(chargeType == "Felony") {
-        res.redirect('quote.html');
+        res.redirect('estf.html');
       } else if(chargeType == "Misdemeanor") {
-        res.redirect('quotemis.html');
+        res.redirect('estm.html');
       } else {
         res.redirect('/');
       }
     });
 });    
+
+// POST request to email index.html form to Jon
+router.post("/est", (req, res) => {
+  console.log(req.body);
+  const output = `
+    <p>New customer requested a call</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.firstName} ${req.body.lastName}</li>
+      <li>City of Residence: ${req.body.city}</li>
+      <li>Phone: ${req.body.phone}</li>
+      <li>Best Time: ${req.body.bestTime}</li>
+      <li>DOB: ${req.body.dob}</li>
+      <li>Date Arrested: ${req.body.dateArrested}</li>
+      <li>Additional Charges: ${req.body.multipleOff}</li>
+      <li>Prior Convictions: ${req.body.priors}</li>
+    </ul>
+  `;
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'ar3home@gmail.com', // generated ethereal user
+            pass: 'r1tspy1t'  // generated ethereal password
+        },
+        tls: {
+          rejectUnauthorized:false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"OC Lawyers" <ar3home@gmail.com>', // sender address
+        to: 'ar3home@gmail.com', // list of receivers
+        subject: 'OC Lawyers Lead to Contact ☏', // Subject line
+        text: '', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        res.redirect('thanks.html');
+    });
+});  
 
 module.exports = router;
